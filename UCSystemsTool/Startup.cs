624f48af66c0a757eef496732bf10ec003ca313c
Ralph V <Ralphverspui@live.nl>
2018-03-28 15:01:45 +0200

    Add project files.

---
+++ UCSystemsTool/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UCSystemsTool.Startup))]
+namespace UCSystemsTool
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
